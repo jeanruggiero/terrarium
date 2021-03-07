c1030b8fb54078b179db31a0fb7c410c80afc1e7
Jean Ruggiero <jeanruggiero@gmail.com>
2021-03-07 13:58:38 -0800

    Refactored code to write directly to timestream

--- data_system.py
+++ data_system.py
@@ -1,14 +1,6 @@
-import board
-import busio
-import adafruit_lps2x
-
 import time
-import datetime
-import os
-import signal
-import subprocess
-import requests
-
+from sensors import PressureSensor, TemperatureSensor
+from timestream import TimestreamClient
 
 class DataSystem:
 
@@ -20,58 +12,16 @@ class DataSystem:
             TemperatureSensor(2, 'desk', 'test temp sensor')
         ]
 
+        self.timestream_client = TimestreamClient()
+
     def run(self):
         while True:
-            for sensor in self.sensors:
-                print(sensor.status())
-                sensor.post(self.gateway_url)
+            records = [sensor.record() for sensor in self.sensors]
+            self.timestream_client.write_records(records)
+
+            print(records)
+
             time.sleep(5)
-
-
-class Sensor:
-
-    def __init__(self, sensor_id, type, units, sample_rate, location, description):
-        self.sensor_id = sensor_id
-        self.type = type
-        self.units = units
-        self.sample_rate = sample_rate
-        self.location = location
-        self.description = description
-
-    def sample(self):
-        raise NotImplementedError
-
-    def post(self, url):
-        requests.post(f"{url}{self.sensor_id}/", data={
-            'timestamp': datetime.datetime.now().isoformat(),
-            'value': self.sample()
-        })
-
-    def status(self):
-        return f"{self.type}: {self.sample():.2f} {self.units}"
-
-
-class PressureSensor(Sensor):
-
-    def __init__(self, sensor_id, location, description):
-        super().__init__(sensor_id, 'pressure', 'HPa', 0.5, location, description)
-        self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.sensor = adafruit_lps2x.LPS25(self.i2c)
-
-    def sample(self):
-        return self.sensor.pressure
-
-
-class TemperatureSensor(Sensor):
-
-    def __init__(self, sensor_id, location, description):
-        super().__init__(sensor_id, 'temperature', 'degC', 0.5, location, description)
-        self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.sensor = adafruit_lps2x.LPS25(self.i2c)
-
-    def sample(self):
-        return self.sensor.temperature
-
 
 
 if __name__ == "__main__":
