0c20bdab31907fd7de7356c591d65e72c0aa2322
Jean Ruggiero <jeanruggiero@gmail.com>
2021-03-07 11:32:02 -0800

    Refactored sensors into separate classes

--- data_system.py
+++ data_system.py
@@ -15,18 +15,63 @@ class DataSystem:
     gateway_url = 'http://raincloud.highlift.io/data/'
 
     def __init__(self):
-        self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.pressure_sensor = adafruit_lps2x.LPS25(self.i2c)
+        self.sensors = [
+            PressureSensor(1, 'desk', 'test pressure sensor'),
+            TemperatureSensor(2, 'desk', 'test temp sensor')
+        ]
 
     def run(self):
         while True:
-            print("Pressure: %.2f hPa" % self.pressure_sensor.pressure)
-            print("Temperature: %.2f C" % self.pressure_sensor.temperature)
-            requests.post(self.gateway_url + '1289/', data={
-                'timestamp': datetime.datetime.now().isoformat(),
-                'value': self.pressure_sensor.temperature
-            })
-            time.sleep(2)
+            for sensor in self.sensors:
+                print(sensor.status())
+                sensor.post(self.gateway_url)
+            time.sleep(5)
+
+
+class Sensor:
+
+    def __init__(self, sensor_id, type, units, sample_rate, location, description):
+        self.sensor_id = sensor_id
+        self.type = type
+        self.units = self.units
+        self.sample_rate = sample_rate
+        self.location = location
+        self.description = description
+
+    def sample(self):
+        raise NotImplementedError
+
+    def post(self, url):
+        requests.post(f"{url}{self.sensor_id}/", data={
+            'timestamp': datetime.datetime.now().isoformat(),
+            'value': self.sample()
+        })
+
+    def status(self):
+        return f"{self.type}: {self.sample():.2f} {self.units}"
+
+
+class PressureSensor(Sensor):
+
+    def __init__(self, sensor_id, location, description):
+        super().__init__(sensor_id, 'pressure', 'HPa', 0.5, location, description)
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.sensor = adafruit_lps2x.LPS25(self.i2c)
+
+    def sample(self):
+        return self.sensor.pressure
+
+
+class TemperatureSensor(Sensor):
+
+    def __init__(self, sensor_id, location, description):
+        super().__init__(sensor_id, 'temperature', 'degC', 0.5, location, description)
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.sensor = adafruit_lps2x.LPS25(self.i2c)
+
+    def sample(self):
+        return self.sensor.temperature
+
 
 
 if __name__ == "__main__":
