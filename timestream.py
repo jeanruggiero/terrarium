79e82a1820ee6c4840a2045e4a5d95937faf83b3
Jean Ruggiero <jeanruggiero@gmail.com>
2021-03-07 14:09:14 -0800

    Added auth info to timestream session and region to client config

--- timestream.py
+++ timestream.py
@@ -1,3 +1,4 @@
+import os
 import boto3
 from botocore.config import Config
 
@@ -8,9 +9,14 @@ class TimestreamClient:
     TABLE_NAME = 'sensor-data'
 
     def __init__(self):
-        session = boto3.Session()
+        session = boto3.Session(
+            aws_access_key_id=os.environ.get('TIMESTREAM_ACCESS_KEY_ID'),
+            aws_secret_access_key=os.environ.get('TIMESTREAM_SECRET_ACCESS_KEY')
+        )
+
         self.client = session.client(
-            'timestream-write', config=Config(read_timeout=20, max_pool_connections=5000, retries={'max_attempts': 10})
+            'timestream-write', config=Config(read_timeout=20, max_pool_connections=5000, retries={'max_attempts': 10},
+                                              region_name="us-west-2")
         )
 
     def write_records(self, records):
