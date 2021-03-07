794f00c93d3e5eba692dd3e5034575042a5aa76d
Jean Ruggiero <jeanruggiero@gmail.com>
2021-03-07 13:57:36 -0800

    Moved sensors into separate file

---
+++ sensors.py
@@ -0,0 +1,57 @@
+import board
+import busio
+import adafruit_lps2x
+
+import time
+
+
+class Sensor:
+
+    def __init__(self, sensor_id, type, units, sample_rate, location, description):
+        self.sensor_id = sensor_id
+        self.type = type
+        self.units = units
+        self.sample_rate = sample_rate
+        self.location = location
+        self.description = description
+
+    def sample(self):
+        raise NotImplementedError
+
+    def record(self):
+
+        return {
+            'MeasureName': str(self.sensor_id),
+            'MeasureValue': f"{self.sample():.4f}",
+            'MeasureValueType': 'DOUBLE',
+            'Time': self.time()
+        }
+
+    def status(self):
+        return f"{self.type}: {self.sample():.2f} {self.units}"
+
+    @staticmethod
+    def time():
+        return str(round(time.time() * 1000))
+
+
+class PressureSensor(Sensor):
+
+    def __init__(self, sensor_id, location, description):
+        super().__init__(sensor_id, 'pressure', 'HPa', 0.5, location, description)
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.sensor = adafruit_lps2x.LPS25(self.i2c)
+
+    def sample(self):
+        return self.sensor.pressure
+
+
+class TemperatureSensor(Sensor):
+
+    def __init__(self, sensor_id, location, description):
+        super().__init__(sensor_id, 'temperature', 'degC', 0.5, location, description)
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.sensor = adafruit_lps2x.LPS25(self.i2c)
+
+    def sample(self):
+        return self.sensor.temperature
